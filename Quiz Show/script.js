8d1252835e4899c310113c095892d58a157ff7d0
commdao <62568113+commdao@users.noreply.github.com>
2020-04-17 16:10:55 -0400

    Add files via upload

---
+++ Quiz Show/script.js
@@ -0,0 +1,55 @@
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstchild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {}
+
+const questions = [
+  {
+    question: "Which Japanese record label does NOT have a US Counterpart",
+    answer: [
+      { text: "Avex Inc", correct: true },
+      { text: "Sony Music", correct: false },
+    ],
+  },
+];
